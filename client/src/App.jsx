0f97e8f62cd5e3ff2e44880ec3b1d79401448c07
saichidvi <saichidvigupta@gmail.com>
2024-01-13 01:45:04 +0530

    completed profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
         <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
         <Route path="/about" element={<About></About>}></Route>
-        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route element={<PrivateRoute></PrivateRoute>}>
+          <Route path="/profile" element={<Profile></Profile>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
